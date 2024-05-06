56e008de66e98f4d6aa3b2516cde1ab558878a20
T.Atkins <43272999+tyronegit@users.noreply.github.com>
2024-05-05 22:15:05 -0400

    updated via instructions

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 4;
+        int finishNumber = 20;
+        int evenCount = 0;
+        int oddCount = 0;
+
+        while (number <= finishNumber) {
+            number++;
+            if (! isEvenNumber ( number )) {
+                oddCount++;
+                continue;
+            }
+            System.out.println ("Even number: " + number);
+            evenCount++;
+        }
+        System.out.println ("Total odd numbers found = " + oddCount);
+        System.out.println ("Total even numbers found = " + evenCount);
+    }
+
+    public static boolean isEvenNumber( int number) {
+
+        return (number % 2) == 0;
+    }
 }
